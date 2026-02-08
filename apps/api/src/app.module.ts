a492fafcb953f4740d9a72d5940663f67fe0523a
BENZOOgataga <contact@benzoogataga.com>
2026-02-08 16:59:34 +0100

    feat(api): redirect root path to health endpoint

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,12 +5,13 @@ import { HealthController } from "./health/health.controller";
 import { MarketController } from "./market/market.controller";
 import { MarketService } from "./market/market.service";
 import { PrismaService } from "./prisma/prisma.service";
+import { RootController } from "./root.controller";
 import { WorldController } from "./world/world.controller";
 import { WorldService } from "./world/world.service";
 
 @Module({
   imports: [],
-  controllers: [HealthController, WorldController, CompaniesController, MarketController],
+  controllers: [RootController, HealthController, WorldController, CompaniesController, MarketController],
   providers: [PrismaService, WorldService, CompaniesService, MarketService]
 })
 export class AppModule {}
